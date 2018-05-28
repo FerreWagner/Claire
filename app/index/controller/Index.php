a87c7ab221b9f9372ba2ca146f5d9e4d84acbefe
FerreWagner <18408229270@163.com>
2018-05-28 14:33:48 +0800

    set success

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -5,14 +5,30 @@ use app\index\Common;
 
 class Index extends Common
 {
+    public function _initialize()
+    {
+        parent::_initialize();
+        $cate   = db('category')->field('id, catename')->order('sort', 'desc')->select();
+        
+        $this->view->assign([
+            'cate'   => $cate,
+        ]);
+    }
+    
     public function index()
     {
         $result = db('article')->field('id, title, cate, see, thumb')->paginate(7);
+        if (input('cateid')){
+            $result = db('article')->field('id, title, cate, see, thumb')->where('cate', input('cateid'))->paginate(7);
+        }
+        
         $this->view->assign([
             'result' => $result,
         ]);
+        
         return $this->view->fetch('index');
     }
+    
 
     /**
      * 单页处理
@@ -27,12 +43,13 @@ class Index extends Common
             $data = db('article')->field('id, thumb, title')->find($id);
             if (empty($data)) $data = db('article')->field('id, thumb, title')->find(1);
             
+            //页面初始化
             $next = db('article')->field('id')->find($id + 1);
             $prev = db('article')->field('id')->find($id - 1);
             
             if (empty($next)) $next = $data;
             if (empty($prev)) $prev = $data;
-//             halt($prev);
+            
             $this->view->assign([
                 'data' => $data,
                 'next' => $next,
