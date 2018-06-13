f7e284b0cca544e4086bf349bb269a365bc2ca19
FerreWagner <18408229270@163.com>
2018-06-13 13:43:45 +0800

    composer scw prepare

--- app/analysis/controller/Index.php
+++ app/analysis/controller/Index.php
@@ -7,7 +7,12 @@ class Index extends Common
 {
     public function index()
     {
+        phpinfo();
+        $url = 'http://heater.fsociaty.com';
+        $article = $this->getWebData($url);
+        
         return $this->view->fetch('index');
     }
+    
 
 }
