b85c218716f14cd0aa5fa8bcaebbd60a64c038e6
FerreWagner <18408229270@163.com>
2018-06-19 15:10:33 +0800

    add program3 view

--- app/analysis/controller/Index.php
+++ app/analysis/controller/Index.php
@@ -29,6 +29,29 @@ class Index extends Common
         return $this->view->fetch('index');
     }
     
+    public function programme3()
+    {
+        
+        if (request()->isPost()){
+            //表单 验证
+            
+            $url  = 'http://heater.fsociaty.com';
+            $time = is_numeric(20) ? 20 : 40;   //分词数
+            if (!filter_var($url, FILTER_VALIDATE_URL)) $this->error('不是标准的地址');
+            
+            
+            
+            //输出
+            $article = $this->getWebData($url);
+            $scws = new Scws();
+            $devid = $scws->scws($article, $time, true);
+            
+            halt($devid);
+        }
+        
+        return $this->view->fetch('program3');
+    }
+    
     //function: 1、提取生成词频前n个(var:词汇),使用GD库生成标签云;TIPS:图片大小；图片背景；文字大小；文字颜色(不统一)；文字字体；文字间距
     public function pic()
     {
